182782548c3f451807e327575edc6e1d38c3e5a2
RadekKosiada <kosiadar@gmail.com>
2020-12-06 21:42:24 +0100

    Divide address in seperate lines & add placeholder if no data

--- src/components/Users.js
+++ src/components/Users.js
@@ -7,10 +7,15 @@ function Users(props) {
     <div className="users-list">
        {users.map((user, index) => (
         <div key={index} className="user-container">
-          <p>{user.address}</p>
-          <p>{user.name}</p>
-          
+        <p className={user.name ? "user-name" : "user-name-empty"}>
+          {user.name ? user.name : "Name"}
+        </p>
+        <div className={user.address ? "user-address" : "user-address-empty"}>
+          <p> {user.address.split(",")[0] ? user.address.split(",")[0] : "Address"}</p>
+          <p> {user.address.split(",")[1] ? user.address.split(",")[1] : "City"} </p>
+          <p> {user.address.split(",")[2] ? user.address.split(",")[2] : "Country"} </p>
         </div>
+      </div>
       ))}
       
     
